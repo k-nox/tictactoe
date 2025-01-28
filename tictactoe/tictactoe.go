ddb118c2f915b92269ef0036bb343cb929186a05
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-28 14:04:06 -0800

    add colors

--- tictactoe/tictactoe.go
+++ tictactoe/tictactoe.go
@@ -17,8 +17,9 @@ type Game struct {
 }
 
 type player struct {
-	name string
-	mark string
+	name  string
+	mark  string
+	color func(s string) string
 }
 
 type move struct {
@@ -36,33 +37,37 @@ func New(r io.Reader) Game {
 			{" ", " ", " "},
 		},
 		player1: player{
-			mark: "X",
+			color: magenta,
 		},
 		player2: player{
-			mark: "O",
+			color: cyan,
 		},
 	}
 
-	game.player1.name = game.playerInput("Player 1, please enter your name: ", func(s string) bool {
+	game.player1.name = game.playerInput(game.player1.color("Player 1, please enter your name: "), func(s string) bool {
 		if len(s) < 1 {
-			fmt.Print("Please enter a name at least 1 character long: ")
+			fmt.Print(red("Please enter a name at least 1 character long: "))
 			return false
 		}
 		return true
 	})
 
-	game.player2.name = game.playerInput("Player 2, please enter your name: ", func(s string) bool {
+	game.player1.mark = game.getMarkForPlayer("X", game.player1)
+
+	game.player2.name = game.playerInput(game.player2.color("Player 2, please enter your name: "), func(s string) bool {
 		if len(s) < 1 {
-			fmt.Print("Please enter a name at least 1 character long: ")
+			fmt.Print(red("Please enter a name at least 1 character long: "))
 			return false
 		}
 		if s == game.player1.name {
-			fmt.Print("Please choose a different name than player 1: ")
+			fmt.Print(red("Please choose a different name than player 1: "))
 			return false
 		}
 
 		return true
 	})
+
+	game.player2.mark = game.getMarkForPlayer("O", game.player2)
 
 	return game
 }
@@ -93,13 +98,13 @@ func (g Game) Play() {
 		g.board[move.row][move.col] = move.mark
 		fmt.Println(g)
 		if g.isWon(move) {
-			fmt.Printf("Congrats to the winner %s!\n", activePlayer.name)
+			fmt.Printf(green("Congrats to the winner %s!\n"), activePlayer.name)
 			return
 		}
 
 		isPlayer1 = !isPlayer1
 	}
-	fmt.Println("It's a draw!")
+	fmt.Println(blue("It's a draw!"))
 }
 
 func (p player) takeTurn(g Game) move {
@@ -107,9 +112,9 @@ func (p player) takeTurn(g Game) move {
 		row int
 		col int
 	)
-	g.playerInput(fmt.Sprintf("%s's turn, pick a cell: ", p.name), func(s string) bool {
+	g.playerInput(p.color(fmt.Sprintf("%s's turn, pick a cell: ", p.name)), func(s string) bool {
 		if len(s) != 2 {
-			fmt.Printf("Please enter two characters: ")
+			fmt.Print(red("Please enter two characters: "))
 			return false
 		}
 
@@ -127,12 +132,12 @@ func (p player) takeTurn(g Game) move {
 		}
 
 		if rawRow != "a" && rawRow != "b" && rawRow != "c" {
-			fmt.Print("Please indicate row with a, b, or c: ")
+			fmt.Print(red("Please indicate row with a, b, or c: "))
 			return false
 		}
 
 		if rawCol != "1" && rawCol != "2" && rawCol != "3" {
-			fmt.Print("Please indicate column with 1, 2, or 3: ")
+			fmt.Print(red("Please indicate column with 1, 2, or 3: "))
 			return false
 		}
 
@@ -147,7 +152,7 @@ func (p player) takeTurn(g Game) move {
 
 		col, _ = strconv.Atoi(rawCol)
 		if g.board[row][col-1] != " " {
-			fmt.Print("That cell has already been played, please pick another one: ")
+			fmt.Print(red("That cell has already been played, please pick another one: "))
 			return false
 		}
 		return true
@@ -217,4 +222,46 @@ func (g Game) playerInput(prompt string, validator func(s string) bool) string {
 		response = g.scanner.Text()
 	}
 	return response
+}
+
+func (g Game) getMarkForPlayer(defaultMark string, player player) string {
+	mark := g.playerInput(player.color(fmt.Sprintf("Hit enter to use %s or type a custom mark now", defaultMark)), func(s string) bool {
+		if len(s) > 1 {
+			fmt.Print(red("Please input only one character"))
+			return false
+		}
+		return true
+	})
+
+	if mark == "" {
+		mark = defaultMark
+	}
+
+	return player.color(mark)
+}
+
+const reset = "\033[0m"
+
+func red(s string) string {
+	return "\033[31m" + s + reset
+}
+
+func green(s string) string {
+	return "\033[32m" + s + reset
+}
+
+func yellow(s string) string {
+	return "\033[33m" + s + reset
+}
+
+func blue(s string) string {
+	return "\033[34m" + s + reset
+}
+
+func magenta(s string) string {
+	return "\033[35m" + s + reset
+}
+
+func cyan(s string) string {
+	return "\033[36m" + s + reset
 }
